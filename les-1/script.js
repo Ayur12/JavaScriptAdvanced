3d991cd3197b0861f9caac3a4f81b24d7bc7a343
Ayur12 <Ayur12@mail.ru>
2021-05-26 14:58:44 +0800

    HomeWork-1

--- les-1/script.js
+++ les-1/script.js
@@ -1,26 +1,34 @@
 const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-  ]
-  
-  const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`
-  }
-  
-  const renderGoodsList = list => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price))
-  
+    { img: 'img/shirt.jpg', title: 'Shirt', price: 150 },
+    { img: 'img/socks.jpg', title: 'Socks', price: 50 },
+    { img: 'img/jacket.jpg', title: 'Jacket', price: 350 },
+    { img: 'img/shoes.webp', title: 'Shoes', price: 250 },
+    { img: 'img/shirt.jpg', title: 'Shirt', price: 150 },
+    { img: 'img/socks.jpg', title: 'Socks', price: 50 },
+    { img: 'img/jacket.jpg', title: 'Jacket', price: 350 },
+    { img: 'img/shoes.webp', title: 'Shoes', price: 250 },
+    { img: 'img/shirt.jpg', title: 'Shirt', price: 150 },
+    { img: 'img/socks.jpg', title: 'Socks', price: 50 },
+    { img: 'img/jacket.jpg', title: 'Jacket', price: 350 },
+    { img: 'img/shoes.webp', title: 'Shoes', price: 250 },
+]
+
+const renderGoodsItem = (img, title, price) => {
+    return `<div class="goods-item"><div class="photo-product"><img src="${img}" alt="photo" width=100%></div><h3>${title}</h3><p class = "price">Price: ${price}</p><button class="add-basket">Добавить</button></div>`
+}
+
+const renderGoodsList = list => {
+    let goodsList = list.map(item => renderGoodsItem(item.img, item.title, item.price)).join('')
+
     //   const goodsListDiv = document.querySelector('.goods-list')
     //   goodsListDiv.innerHTML = goodsList
-  
+
     document.querySelector('.goods-list').innerHTML = goodsList
-  
-  }
-  
-  const init = () => {
+
+}
+
+const init = () => {
     renderGoodsList(goods)
-  }
-  
-  window.onload = init
+}
+
+window.onload = init
