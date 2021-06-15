7351818a7dde1e7bfdb03b0554ee426d561ecfb0
Ayur12 <Ayur12@mail.ru>
2021-06-15 16:53:06 +0800

    les-6

--- les-6 компоненты Vue.js/HW-6/script.js
+++ les-6 компоненты Vue.js/HW-6/script.js
@@ -1,40 +1,70 @@
 const API_URL =
     "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses";
 
-
+Vue.component('custom-input', {
+    props: ['value'],
+    template: `        
+        <input class = 'custom-input'
+        placeholder = 'введите запрос'
+        v-bind:value='searchLine'
+        v-on:input="$emit('input', $event.target.value)"
+        v-on:keyup.enter="submit"
+        >      
+        `
+});
 
 
 Vue.component('goods-list', {
     props: ['goods'],
     template: `
         <div class = "goods-list">
-        <goods-item v-for = "goodEntity in goods" :goodProp = "goodEntity"></goods-item>
+            <goods-item v-for = "goodEntity in goods" :goodProp = "goodEntity"></goods-item>
         </div>
     `
 });
 
 Vue.component('goods-item', {
-    props: ['goodProp', "addBasket"],
+    data: {
+        addBasket(clicked_id) {
+            let btn_id = clicked_id;
+            this.goods.forEach((good) => {
+                if (good.id_product === btn_id) {
+                    this.basketList.push(good);
+                }
+            });
+            document.getElementById(clicked_id).textContent = 'Добавлено';
+            document.getElementById(clicked_id).disabled = true;
+
+        },
+    },
+    props: ['goodProp'],
     template: `
         <div class = "goods-item">
             <h3>{{goodProp.product_name}}</h3>
             <p>{{goodProp.price}}</p>
-            <button :id='goodProp.id_product' @click='addBasket'>В корзину</button>
+            <button class = "btnAdd" :id='goodProp.id_product' @click='addBasket(goodProp.id_product)'>В корзину</button>
         </div>
     `
 });
 
-Vue.component('custom-input', {
-    props: ['value'],
+Vue.component('basket-list', {
+    props: ['basketList'],
     template: `
-        <input class = 'custom-input'
-        v-bind:value='searchLine'
-        v-on:input="$emit('input', $event.target.value)"
-        v-on:keyup.enter="submit"
-      >      
+        <div class = "basket-list">
+        <basket-item v-for = "goodEntity in basketList" :goodProp = "goodEntity"></basket-item>
+        </div>
     `
 });
 
+Vue.component('basket-item', {
+    props: ['goodProp'],
+    template: `
+        <div class = "basket-item">
+            <h3>{{goodProp.product_name}}</h3>
+            <p>{{goodProp.price}}</p>            
+        </div>
+    `
+});
 
 
 const app = new Vue({
@@ -55,12 +85,13 @@ const app = new Vue({
                 const catalogItems = await responce.json();
                 this.goods = catalogItems;
                 this.filteredGoods = catalogItems;
+                //this.basketList = catalogItems;
             } else {
                 alert("Ошибка при соединении с сервером");
             }
         },
 
-        filterGoods() {
+        filterGoods: function () {
             this.searchLine = document.querySelector('.custom-input').value;
             let userInput = this.searchLine;
             this.goods.forEach((good) => {
@@ -71,13 +102,17 @@ const app = new Vue({
             });
         },
 
-        addBasket() {
+        addBasket: function (clicked_id) {
+            let btn_id = clicked_id;
+            this.goods.forEach((good) => {
+                if (good.id_product === btn_id) {
+                    this.basketList.push(good);
+                }
+            });
+            document.getElementById(clicked_id).textContent = 'Добавлено';
+            document.getElementById(clicked_id).disabled = true;
 
-            console.log('testClick')
-
-
-
-        }
+        },
     },
 
     async mounted() {
